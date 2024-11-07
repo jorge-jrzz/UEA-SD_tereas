5842032fe3edf074679a40cf14a40ce434b82d1f
paprikadreamdetective <jorge.nicolas@cua.uam.mx>
2024-11-06 22:34:11 -0500

    Clean up commented-out code

--- p1_sockets/server.py
+++ p1_sockets/server.py
@@ -21,18 +21,14 @@ if __name__ == '__main__':
         print('Esperando conexión...')
         connection, client_address = sock.accept()
         print('Conectado a', client_address)
-
         try:
             while True:
                 data = connection.recv(1024)
                 if not data:
                     print("El cliente se ha desconectado.")
                     break
-
                 message = data.decode()
                 print('Mensaje recibido:', message)
-                
-
                 if message == 'estado':
                     respuesta = f'0,{ganancia_total},{num_viajes}'
                     connection.sendall(respuesta.encode())
@@ -43,7 +39,6 @@ if __name__ == '__main__':
                             uber.set_availability(False)  # Marcar el Uber como ocupado
                             current_plate = uber.get_plate()
                             break
-
                     if current_plate == -1:
                         respuesta = "-1,No hay autos disponibles"
                     else:
@@ -52,86 +47,17 @@ if __name__ == '__main__':
                         ganancia_total += precio_viaje
                         num_viajes += 1
                         connection.sendall(respuesta.encode())
-                    
                 elif message.startswith('terminar'):
                     plate_to_free = int(message.split(',')[1])
                     available_uber[plate_to_free].set_availability(True)  # Liberar el Uber
                     respuesta = f'Uber {plate_to_free} está disponible nuevamente.'
                     print(respuesta)
                     connection.sendall(respuesta.encode())  # Enviar confirmación al cliente
-
                 else:
                     respuesta = "Comando no reconocido"
                     connection.sendall(respuesta.encode())
-                    
         except Exception as e:
             print('Error con el cliente:', e)
         finally:
             connection.close()
 
-
-
-'''
-from uber import Uber
-
-import socket
-import random
-
-SERVER_IP = '0.0.0.0'
-SERVER_PORT = 2020
-
-if __name__ == '__main__': 
-    ganancia_total = 0.0
-    num_viajes = 0
-    
-    available_uber = [Uber(i, True) for i in range(0, 5)]
-    
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_address = (SERVER_IP, SERVER_PORT)
-    print('Iniciando en {} puerto {}'.format(*server_address))
-    sock.bind(server_address)
-
-    sock.listen(10)
-
-    print('Esperando una conexion...')
-    # connection, client_address = sock.accept()
-    # print('Conectado a', client_address)
-
-    
-
-    while True:
-        print('Esperando conexion . . .')
-        connection, client_address = sock.accept()
-        print('Conectado a', client_address)
-
-        try:
-            while True:
-                data = connection.recv(1024)
-                if not data:
-                    print("El cliente se ha desconectado.")
-                    break
-
-                print('Mensaje recibido:', data.decode())
-    
-                if data.decode() == 'estado':
-                    respuesta = f'0,{ganancia_total},{num_viajes}'
-                    connection.sendall(respuesta.encode())
-                elif data.decode() == 'viaje':
-                    current_plate = -1
-                    for uber in available_uber:
-                        if uber.is_available():
-                            uber.set_availability(False)
-                            current_plate = uber.get_plate()
-                            break
-                    respuesta = f'1,{current_plate},{random.randint(23, 100)}'
-                    connection.sendall(respuesta.encode())
-                elif data.decode().split(',')[0] == 'terminar':
-                    available_uber[int(data.decode().split(',')[1])].set_availability(True)
-                else:
-                    respuesta = "Comando no reconocido"
-                    connection.sendall(respuesta.encode())
-        except Exception as e:
-            print('Error con el cliente: ', e)
-        finally:
-            connection.close()
-'''        
