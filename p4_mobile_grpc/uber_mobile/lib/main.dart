52f2f3d07078b28d0dee79227e123cd65066aef2
Jorge Ángel Juárez Vázquez <jorgeang33@gmail.com>
2024-11-29 19:15:06 -0600

    🚧 Update buttons

--- p4_mobile_grpc/uber_mobile/lib/main.dart
+++ p4_mobile_grpc/uber_mobile/lib/main.dart
@@ -36,7 +36,7 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Uber Clone'),
+        title: Image.asset('assets/images/uber_logo.png', height: 30.0,), 
         centerTitle: true,
       ),
       body: Column(
@@ -46,12 +46,13 @@ class _HomePageState extends State<HomePage> {
             flex: 1,
             child: FlutterMap(
               options: MapOptions(
-                initialCenter: LatLng(19.432608, -99.133209), // Ciudad de México
+                initialCenter:
+                    LatLng(19.432608, -99.133209), // Ciudad de México
                 initialZoom: 12.0,
               ),
               children: [
                 TileLayer(
-                  urlTemplate: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+                  urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
                   subdomains: ['a', 'b', 'c'],
                 ),
               ],
@@ -63,31 +64,58 @@ class _HomePageState extends State<HomePage> {
             child: Column(
               children: [
                 const SizedBox(height: 10),
-                _buildOptionButton('UberPlanet'),
-                _buildOptionButton('UberXL'),
-                _buildOptionButton('UberBlack'),
+                _buildOptionButton(
+                  'assets/images/uber_planet.png',
+                  'UberPlanet', 
+                  'Viaje compartido'),
+                _buildOptionButton(
+                  'assets/images/uber_xl.png', 
+                  'UberXL', 
+                  'Viaje compartido'),
+                _buildOptionButton(
+                  'assets/images/uber_black.png', 
+                  'UberBlack', 
+                  'Viaje compartido'),
                 const Spacer(),
                 if (selectedOption != null)
                   Padding(
                     padding: const EdgeInsets.all(16.0),
-                    child: ElevatedButton(
-                      onPressed: () {
-                        // Aquí manejas la confirmación del viaje
-                        ScaffoldMessenger.of(context).showSnackBar(
-                          SnackBar(
-                            content: Text('Has seleccionado $selectedOption'),
+                    child: SizedBox(
+                        width: double.infinity,
+                        child: ElevatedButton(
+                          onPressed: () {
+                            // Muestra el dialogo de confirmación
+                            showDialog(
+                              context: context,
+                              builder: (BuildContext context) {
+                                return AlertDialog(
+                                  title: const Text('Confirmación'),
+                                  content: Text('Has seleccionado $selectedOption'),
+                                  actions: [
+                                    TextButton(
+                                      onPressed: () {
+                                        Navigator.of(context).pop(); // Cerrar el diálogo
+                                      },
+                                      child: const Text('OK'),
+                                    ),
+                                  ],
+                                );
+                              },
+                            );
+                          },
+                          style: ElevatedButton.styleFrom(
+                            padding: const EdgeInsets.symmetric(vertical: 14.0),
+                            backgroundColor: Colors.blue[600],
                           ),
-                        );
-                      },
-                      style: ElevatedButton.styleFrom(
-                        padding: const EdgeInsets.symmetric(vertical: 16.0),
-                        backgroundColor: Colors.black,
+                          child: const Text(
+                            'Solicitar viaje',
+                            style: TextStyle(
+                              fontSize: 16, 
+                              fontWeight: FontWeight.bold, 
+                              color: Colors.white),
+                          ),
+                        ),
                       ),
-                      child: const Text(
-                        'Confirmar viaje',
-                        style: TextStyle(fontSize: 18),
-                      ),
-                    ),
                   ),
               ],
             ),
@@ -98,22 +126,56 @@ class _HomePageState extends State<HomePage> {
   }
 
   // Widget para un botón de opción
-  Widget _buildOptionButton(String option) {
+  Widget _buildOptionButton(String imagePath, String option, String details) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-      child: ElevatedButton(
-        onPressed: () {
+      child: GestureDetector(
+        onTap: () {
           setState(() {
             selectedOption = option;
           });
         },
-        style: ElevatedButton.styleFrom(
-          padding: const EdgeInsets.symmetric(vertical: 16.0),
-          backgroundColor: selectedOption == option ? Colors.blue : Colors.grey,
-        ),
-        child: Text(
-          option,
-          style: const TextStyle(fontSize: 18),
+        child: Container(
+          decoration: BoxDecoration(
+            color: Colors.white, // Fondo blanco
+            border: Border.all(
+              color: selectedOption == option
+                  ? Colors.blue
+                  : Colors.black, // Borde negro (o azul si está seleccionado)
+              width: 2.0,
+            ),
+            borderRadius: BorderRadius.circular(8.0), // Bordes redondeados
+          ),
+          padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 16.0),
+          child: Row(
+            children: [
+              // Imagen a la izquierda
+              Image.asset(
+                imagePath,
+                width: 50.0,
+              ),
+              const SizedBox(width: 30), // Espacio entre imagen y texto
+              // Texto a la derecha
+              Text(
+                option,
+                style: const TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black, // Texto en negro
+                ),
+              ),
+              const Spacer(), // Espacio entre el nombre del Uber y texto de los detalles
+              // Texto de los detalles del precio del servicio
+              Text(
+                details,
+                style: const TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.normal,
+                  color: Colors.grey, // Texto en negro
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
