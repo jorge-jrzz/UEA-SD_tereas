d04c3827f1c594f4c8e07fdcce752b988987f9e7
paprikadreamdetective <jorge.nicolas@cua.uam.mx>
2024-12-02 21:32:13 -0600

    🎉 Successfully connected the admin page to the backend
    Successfully connected the admin page to the backend

--- p4_mobile_grpc/uber_mobile/lib/src/pages/administrator_screen.dart
+++ p4_mobile_grpc/uber_mobile/lib/src/pages/administrator_screen.dart
@@ -5,72 +5,6 @@ import 'package:grpc/grpc.dart';
 import '../protos/ride_service.pbgrpc.dart';
 
 //import 'package:ride_service/src/generated/ride_service.pbgrpc.dart'; // Asegúrate de que este sea el path correcto
-/*
-class AdministratorScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Información del Servicio'),
-        centerTitle: true,
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const Text(
-              'Número de viajes realizados:',
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            const SizedBox(height: 10),
-            Text(
-              'numero de viajes aqui',
-              style: TextStyle(fontSize: 17, backgroundColor: Colors.grey[300]),
-            ),
-            const Divider(),
-            const Text(
-              'Número de autos libres:',
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            const SizedBox(height: 10),
-            Text(
-              'numero de autos aqui',
-              style: TextStyle(fontSize: 17, backgroundColor: Colors.grey[300]),
-            ),
-            const Divider(),
-            const Text(
-              'Ganancias totales:',
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            const SizedBox(height: 10),
-            Text(
-              '\$ ganancias aqui',
-              style: TextStyle(fontSize: 17, backgroundColor: Colors.grey[300]),
-            ),
-            const Divider(),
-            const Spacer(),
-            ButtonFuntion(
-                text: 'Volver',
-                color: Colors.black,
-                onPressed: () {
-                  Navigator.pop(context);
-                })
-          ],
-        ),
-      ),
-    );
-  }
-}*/
 class AdministratorScreen extends StatefulWidget {
   @override
   _AdministratorScreenState createState() => _AdministratorScreenState();
@@ -88,17 +22,11 @@ class _AdministratorScreenState extends State<AdministratorScreen> {
     _initializeClient();
     _fetchServiceInfo();
   }
-  /*
-  void _initializeClient() {
-    final channel =
-        GrpcWebClientChannel.xhr(Uri.parse('http://localhost:50051'));
-    _client = RideServiceClient(channel);
-  }*/
 
   void _initializeClient() {
     final channel = ClientChannel(
-      'localhost', // Dirección del servidor
-      port: 50051, // Puerto de gRPC
+      'localhost',
+      port: 50051,
       options: const ChannelOptions(
         credentials:
             ChannelCredentials.insecure(), // Si no tienes SSL, usa esto
@@ -107,25 +35,6 @@ class _AdministratorScreenState extends State<AdministratorScreen> {
     );
     _client = RideServiceClient(channel);
   }
-
-  /*
-  Future<void> _fetchServiceInfo() async {
-    try {
-      final response = await _client.getServiceInfo(EmptyRequest());
-      setState(() {
-        _totalTrips = response.totalTrips.toString();
-        _availableCars = response.availableCars.toString();
-        _earnings = "\$${response.earnings.toStringAsFixed(2)}";
-      });
-    } catch (e) {
-      setState(() {
-        _totalTrips = "Error";
-        _availableCars = "Error";
-        _earnings = "Error";
-      });
-      print("Error fetching service info: $e");
-    }
-  }*/
 
   Future<void> _fetchServiceInfo() async {
     try {
@@ -192,7 +101,7 @@ class _AdministratorScreenState extends State<AdministratorScreen> {
             ),
             const SizedBox(height: 10),
             Text(
-              _earnings,
+              "${_earnings}",
               style: TextStyle(fontSize: 17, backgroundColor: Colors.grey[300]),
             ),
             const Divider(),
